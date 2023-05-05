e6f6184cf77bf4a1f49d92f70f4e225b093dc17b
mrGolbya <mr.Golbya@gmail.com>
2023-05-05 22:16:35 +0300

    fix: removed unused variable

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const read = fs.createReadStream(path.resolve(__dirname, './text.txt'),'utf-8');
 
-let data = '';
-
 read.on('data', chunk => console.log(chunk));
-read.on('end', () => console.log('End', data));
+read.on('end', () => console.log('End'));
 read.on('error', error => console.log('Error', error.message));
